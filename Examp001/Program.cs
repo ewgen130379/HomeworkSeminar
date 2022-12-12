da10db75df147d382923d9ca4b7c7dc090fea319
ewgen130379 <118625594+ewgen130379@users.noreply.github.com>
2022-12-12 08:53:45 +0300

    Examp005

---
+++ Examp001/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Start");
+int a = 4;
+int b = 3;
+int c = 21;
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    max = max;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine(max);// Вывод результата
+Console.WriteLine("end");
---
+++ Examp003/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 3: Напишите программу, которая на вход принимает число (N),а на выходе показывает все чётные числа от 1 до N.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("введите число:");
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+        Console.ReadKey();
+    }
+}
